3c64c64ad28d84bf5eca2d0fda28ecafcbdf0763
Taeho Kang <kangth97@gmail.com>
2024-12-18 22:34:17 +0900

    first commit

---
+++ src/main/java/app/ebel/staedybucks/StaedybucksApplication.java
@@ -0,0 +1,13 @@
+package app.ebel.staedybucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaedybucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaedybucksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/ebel/staedybucks/StaedybucksApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ebel.staedybucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaedybucksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
