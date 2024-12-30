48f4ce3e62d1d91d933880b4005f803b767d197c
Taeho Kang <kangth97@gmail.com>
2024-12-31 00:54:16 +0900

    swagger, querydsl에 대한 dependency, config 추가

---
+++ src/main/java/app/ebel/staedybucks/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package app.ebel.staedybucks.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("SteadyBucks API")
+                        .version("0.1-alpha")
+
+                );
+    }
+}
