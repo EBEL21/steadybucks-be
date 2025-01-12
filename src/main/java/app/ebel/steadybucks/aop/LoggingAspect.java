0e9fde9ab057eb8a0f01f8969a69192cadd2f0f7
Taeho Kang <kangth97@gmail.com>
2025-01-12 23:42:09 +0900

    로깅 형식 수정

--- src/main/java/app/ebel/steadybucks/aop/LoggingAspect.java
+++ src/main/java/app/ebel/steadybucks/aop/LoggingAspect.java
@@ -19,7 +19,7 @@ public class LoggingAspect {
     private final HttpServletRequest request;
 
     @Around("execution(* app.ebel.steadybucks.controller..*(..))")
-    public Object logApiCall(ProceedingJoinPoint joinPoint) throws  Throwable {
+    public Object logApiCall(ProceedingJoinPoint joinPoint) throws Throwable {
         long startTime = System.currentTimeMillis();
 
         // API 요청 정보
@@ -30,8 +30,8 @@ public class LoggingAspect {
         String sessionId = request.getRequestedSessionId();
         String user = request.getRemoteUser() != null ? request.getRemoteUser() : "Anonymous";
 
-        logger.info("API Call Started: [Method: {}, URL: {}, Query: {}, IP: {}, Session: {}, User: {}]",
-                method, url, queryParams, ip, sessionId, user);
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
 
         Object result;
         int statusCode = 200; // 기본 상태 코드
@@ -44,15 +44,15 @@ public class LoggingAspect {
             }
         } catch (Exception e) {
             long endTime = System.currentTimeMillis();
-            logger.error("API Call Failed: [Method: {}, URL: {}, IP: {}, Error: {}, Execution Time: {} ms]",
-                    method, url, ip, e.getMessage(), (endTime - startTime), e);
+            logger.error("[USER: {}, SESSION: {}, METHOD: {}, URL: {}, FROM: {} IP: {}, EX: {}, MSG: {}, Execution Time: {}ms]",
+                    user, sessionId, method, url, ip, className + '.' + methodName, e.getClass().getSimpleName(), e.getMessage(), (endTime - startTime));
 
             throw e;
         }
 
         long endTime = System.currentTimeMillis();
-        logger.info("API Call Completed: [Method: {}, URL: {}, StatusCode: {}, Execution Time: {} ms]",
-                method, url, statusCode, (endTime - startTime));
+        logger.info("[USER: {}, SESSION: {}, METHOD: {}, URL: {}, IP: {}, StatusCode: {}, Execution Time: {}ms]",
+                user, sessionId, method, url, ip, statusCode, (endTime - startTime));
 
         return result;
     }
